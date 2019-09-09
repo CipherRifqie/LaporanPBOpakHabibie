e3e6cb8346f53029611091e6dac0dd7c40da48ea
CipherRifqie <47323406+CipherRifqie@users.noreply.github.com>
2019-09-09 07:03:24 +0700

    Update Mahasiswa.java

--- src/2_Class_dan_Object/Mahasiswa.java
+++ src/2_Class_dan_Object/Mahasiswa.java
@@ -1,4 +1,4 @@
-public class Mahasiswa {
+public class Mahasiswa1841720065Rifqie {
     public int nim;
     public String nama;
     public String alamat;
@@ -6,7 +6,7 @@ public class Mahasiswa {
     public int nomorTelepon;
     public String asalNegara;
     
-    public void tampilBiodata(){
+    public void tampilBiodata1841720065Rifqie(){
         System.out.println("NIM : " +nim);
         System.out.println("Nama : " +nama);
         System.out.println("Alamat : " +alamat);
