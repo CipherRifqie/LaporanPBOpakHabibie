102b3a603563a8fba92980d79625b55f86a80993
CipherRifqie <47323406+CipherRifqie@users.noreply.github.com>
2019-09-09 22:32:19 +0700

    Update TestMahasiswa1841720065Rifqie.java

--- src/2_Class_dan_Object/Percobaan2/TestMahasiswa1841720065Rifqie.java
+++ src/2_Class_dan_Object/Percobaan2/TestMahasiswa1841720065Rifqie.java
@@ -5,8 +5,6 @@ public class TestMahasiswa1841720065Rifqie {
         mhs1.nama = "Lestari";
         mhs1.alamat="Jl. Vinolia no 1A";
         mhs1.kelas="1A";
-        mhs1.nomorTelepon = 1298555456;
-        mhs1.asalNegara = "Indonesia";
         mhs1.tampilBiodataRifqie();
 }
 }
